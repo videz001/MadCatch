e846e642fcbbe986a000aad0512c8311557864bd
videz001 <166196907+videz001@users.noreply.github.com>
2025-08-21 17:34:23 +0800

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,7 +1,7 @@
 # To learn more about how to use Nix to configure your environment
 # see: https://firebase.google.com/docs/studio/customize-workspace
 {pkgs}: {
-  # Which nixpkgs channel to use.
+   # Which nixpkgs channel to use.
   channel = "stable-24.11"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
